e7813a260bfdcbf812dfde8db3eeb03e0eb9ddef
Maleesha Ranawaka <umranawaka@gmail.com>
2025-09-03 11:56:21 +0530

    Added auto changing date to footer.js

--- footer.js
+++ footer.js
@@ -1,4 +1,9 @@
 (function injectFooter(){
+  const now = new Date();
+
+  const options = { day: '2-digit', month: 'short', year: 'numeric' };
+  const formattedDate = now.toLocaleDateString('en-GB', options);
+
   var el = document.getElementById("footer");
   if (!el) {
     el = document.createElement("div");
@@ -11,7 +16,7 @@
         <div class="row">
           <div class="col-lg-12 col-12">
             <p class="copyright-text">
-              <h6>  Mini Finance v1.0 — Deployed on 01 Sep 2025 — By Maleesha Ranawaka </h6>
+              <h6>  Mini Finance v1.0 — Deployed on ${formattedDate} — By Maleesha Ranawaka </h6>
               Copyright © Mini Finance 2048 - Design:
               <a rel="sponsored" href="https://www.tooplate.com" target="_blank">Tooplate</a>
             </p>
